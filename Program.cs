a4d43edaa813f3208e4eda8be11431bdc06a37e6
iwaniukd4 <iwaniukd4@gmail.com>
2026-07-07 17:44:00 +0200

    Updated to serve vue.js app

--- Program.cs
+++ Program.cs
@@ -1,41 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Logistics
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder =
+                WebApplication.CreateBuilder(args);
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddSpaStaticFiles(
+                configuration =>
+            {
+#if DEBUG
+                configuration.RootPath = "ClientApp";
+#else
+                configuration.RootPath =
+                    "ClientApp/dist/spa";
+#endif
+            });
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowLocalDevelopmentOrigin",
+                    policy => policy
+                        .WithOrigins(
+                            "http://localhost:8080"
+                        )
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+            });
+            app.UseCors("AllowLocalDevelopmentOrigin");
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
